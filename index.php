32b01d6065817ee1fa41af27aeadbe581c940ac6
Stacey Lane <stacey@tune.com>
2015-08-28 16:47:53 -0700

    Init
    Rough version of the tune.com framework.

---
+++ index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>%%title%%</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!--  Favicon -->
+		<link rel="shortcut icon" href="http://www.tune.com/favicon.ico?v=2" />
+
+		<link rel="stylesheet" type="text/css" href="//cloud.typography.com/6536912/638684/css/fonts.css" />
+		<link href="/css/2016-pardot-lp-templates-framework.css" rel="stylesheet" type="text/css">
+		<link href="/css/2016-pardot-lp-templates-styles.css" rel="stylesheet" type="text/css">
+		
+		<script src="/js/vendor/modernizr-2.6.2.min.js"></script>
+	</head>
+
+	<body>
+
+		<!-- SITE BRANDING // -->
+		<header class="banded banded--nowhitespace site-branding">
+			<div class="row">
+				<div class="col-100 text-centered">
+					<a href="/">Tune</a>
+				</div>
+			</div>
+		</header>
+		<!-- // SITE BRANDING -->
+
+		<!-- HERO IMAGE // -->
+		<section class="hero">
+			<img src="http://placehold.it/1200x200/007aff/ffffff" pardot-region="region__hero-image--1200x200">
+		</section>		
+		<!-- // HERO IMAGE -->
+
+		<section>
+			<div class="banded banded--lesswhitespace row">
+				<div class="col-100">
+
+					<div class="hgroup">
+						<h2>Whitepaper: App Store Optimization</h2>
+						<h1>The Ultimate Guide to Keyword Research</h1>
+					</div>
+
+
+
+
+
+
+<hr style="background: white; margin: 40px 0;">
+
+
+<h1>This is a heading 1. Let's talk about the rest of the text you should style.</h1>
+		<p>This is some paragraph text. <a href="http://www.example.com">This is an example link</a>. You can do a lot of things with a paragraph. Let's check it out. You can make text <b>bold</b>, not to be confused with <strong>strong text</strong>. You can also <em>emphasize</em> text, which appears as <i>italics</i>. Strong and emphasized text are the accessible equivilent of bold and italic tags. It's possible to <u>underline</u> text. It's also  possible to <del>delete</del> <ins>insert</ins> text. Text can be <sup>superscript</sup>, <sub>subscript</sub> and <small>small</small>. All of these are inline elements that can be used inside of the paragraph tag.</p>
+
+
+<h2>This is a heading 2. Don't forget to add a bunch of text so this cascades to multiple lines. Because you want to make sure the line height is right. I'm a poet.</h2>
+		<p>This is some paragraph text. <a href="http://www.example.com">This is an example link</a>. You can do a lot of things with a paragraph. Let's check it out. You can make text <b>bold</b>, not to be confused with <strong>strong text</strong>. You can also <em>emphasize</em> text, which appears as <i>italics</i>. Strong and emphasized text are the accessible equivilent of bold and italic tags. It's possible to <u>underline</u> text. It's also  possible to <del>delete</del> <ins>insert</ins> text. Text can be <sup>superscript</sup>, <sub>subscript</sub> and <small>small</small>. All of these are inline elements that can be used inside of the paragraph tag.</p>
+
+
+<h3>A heading 3. And adding a bunch of filler text for that line height effect. You know what's cool? Code. Code is cool. C'mon, you knew that was coming.</h3>
+		<p>This is some paragraph text. <a href="http://www.example.com">This is an example link</a>. You can do a lot of things with a paragraph. Let's check it out. You can make text <b>bold</b>, not to be confused with <strong>strong text</strong>. You can also <em>emphasize</em> text, which appears as <i>italics</i>. Strong and emphasized text are the accessible equivilent of bold and italic tags. It's possible to <u>underline</u> text. It's also  possible to <del>delete</del> <ins>insert</ins> text. Text can be <sup>superscript</sup>, <sub>subscript</sub> and <small>small</small>. All of these are inline elements that can be used inside of the paragraph tag.</p>
+
+
+<h4>And a heading 4. Seattle is nice this time of year. You should really come and check it out. There's even a rain forest on the Olympic Peninsula (did I spell that right?). And there's also the space needle. If you're into sight seeing. Don't be a chicken. It's only ~520ft up.</h4>
+		<p>This is some paragraph text. <a href="http://www.example.com">This is an example link</a>. You can do a lot of things with a paragraph. Let's check it out. You can make text <b>bold</b>, not to be confused with <strong>strong text</strong>. You can also <em>emphasize</em> text, which appears as <i>italics</i>. Strong and emphasized text are the accessible equivilent of bold and italic tags. It's possible to <u>underline</u> text. It's also  possible to <del>delete</del> <ins>insert</ins> text. Text can be <sup>superscript</sup>, <sub>subscript</sub> and <small>small</small>. All of these are inline elements that can be used inside of the paragraph tag.</p>
+
+
+<h5>We'll go ahead and add a heading 5. Oh man. This text is showing up really tiny and stuff. Ever seen toe-shoes? Some chick was wearing them the other day and it totally blew my mind. They're shoes that have indivdual places for each toe. It's like rubber gloves for your feet.</h5>
+		<p>This is some paragraph text. <a href="http://www.example.com">This is an example link</a>. You can do a lot of things with a paragraph. Let's check it out. You can make text <b>bold</b>, not to be confused with <strong>strong text</strong>. You can also <em>emphasize</em> text, which appears as <i>italics</i>. Strong and emphasized text are the accessible equivilent of bold and italic tags. It's possible to <u>underline</u> text. It's also  possible to <del>delete</del> <ins>insert</ins> text. Text can be <sup>superscript</sup>, <sub>subscript</sub> and <small>small</small>. All of these are inline elements that can be used inside of the paragraph tag.</p>
+
+
+<h6>And let's finish off the headings with a heading 6. All I can say is that my life is pretty plain. I like watching the puddles gather rain. And all I can do is just pour some tea for two and speak my point of view, but it's not sane.</h6>
+		<p>This is some paragraph text. <a href="http://www.example.com">This is an example link</a>. You can do a lot of things with a paragraph. Let's check it out. You can make text <b>bold</b>, not to be confused with <strong>strong text</strong>. You can also <em>emphasize</em> text, which appears as <i>italics</i>. Strong and emphasized text are the accessible equivilent of bold and italic tags. It's possible to <u>underline</u> text. It's also  possible to <del>delete</del> <ins>insert</ins> text. Text can be <sup>superscript</sup>, <sub>subscript</sub> and <small>small</small>. All of these are inline elements that can be used inside of the paragraph tag.</p>
+
+<blockquote>Want to insert a long quote from someone that you really admire? Well you can! And they should be styled as well.</blockquote>
+
+<q>Of course, you can insert a short quote too.</q>
+
+<ol>
+  <li>This is an ordered list</li>
+  <li>It has spiffy numbers</li>
+</ol>
+
+<ul>
+  <li>This is an unordered list</li>
+  <li>Notice the bullets</li>
+</ul>
+
+<hr />
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+				</div>
+			</div>
+		</section>
+	</body>
+</html>
